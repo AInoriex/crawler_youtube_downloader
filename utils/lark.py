63cb8e68e5d3b47545d4e43e04af23735b11a2b0
AInoriex <xiangyuhao@52tt.com>
2024-10-03 06:04:06 +0800

    [bugfix] 飞书告警补充重试逻辑；

--- utils/lark.py
+++ utils/lark.py
@@ -1,31 +1,37 @@
 import requests
-import json
+from time import sleep
 
 # class LarkNotice():
 #     def __init__(self, notice_text) -> None:
 #         self.notice_text = notice_text
 
-def alarm_lark_text(webhook:str, text:str)->bool:
+def alarm_lark_text(webhook:str, text:str, retry:int=3)->bool:
     ''' 飞书普通文本告警 '''
     ''' Expamle Json Send
     {
 	    "msg_type": "text",
 	    "content": {"text": "test hello world."}
     }'''
-    params = {
-	    "msg_type": "text",
-	    "content": {"text": f"{text}"}
-    }
-    # print(f"request: {webhook} | {params}")
-    resp = requests.post(url=webhook, json=params)
-    # print(f"response: {resp.status_code} {resp.content}")
-    if resp.status_code != 200:
-        return False
-    resp = resp.json()
-    if resp["code"] != 0:
-        return False
-    print(f"Lark > 已通知飞书: {resp}")
-    return True
+    try:
+        params = {
+            "msg_type": "text",
+            "content": {"text": f"{text}"}
+        }
+        # print(f"request: {webhook} | {params}")
+        resp = requests.post(url=webhook, json=params)
+        # print(f"response: {resp.status_code} {resp.content}")
+        assert resp.status_code == 200
+        assert resp.json()["code"] == 0
+    except Exception as e:
+        print(f"Lark > [!] 发送飞书失败: {e}")
+        sleep(1)
+        if retry > 0:
+            return alarm_lark_text(webhook=webhook, text=text, retry=retry-1)
+        else:
+            return False
+    else:
+        print(f"Lark > 已通知飞书: {resp}")
+        return True
 
 if __name__ == "__main__":
     webhook = "https://open.feishu.cn/open-apis/bot/v2/hook/34755f1e-5fc7-46c9-9fee-177317a581ee"
