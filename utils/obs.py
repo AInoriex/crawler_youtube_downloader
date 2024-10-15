6bcd6aeaa7c3859af38aa6b19d02b469ba7202e4
AInoriex <xiangyuhao@52tt.com>
2024-10-15 10:50:21 +0800

    [feat] 增加下载&分段上传重试机制；

--- utils/obs.py
+++ utils/obs.py
@@ -1,3 +1,8 @@
+# import sys
+# import os
+# work_dir = os.path.realpath(os.path.dirname(sys.argv[0]))
+# sys.path.append(os.path.join(work_dir, ".."))
+
 # 引入模块
 import os
 from obs import ObsClient
@@ -5,6 +10,7 @@ from obs import PutObjectHeader
 from dotenv import load_dotenv
 from urllib.parse import urljoin
 import traceback
+from time import sleep
 load_dotenv()
 
 # 初始化读取配置文件
@@ -83,10 +89,9 @@ def upload_file(from_path:str, to_path:str)->str:
         return urljoin(os.getenv("OBS_URLBASE"), to_path)
 
     finally:
-        # 关闭obsClient
         obsClient.close()
 
-def upload_file_v2(from_path:str, to_path:str)->str:
+def upload_file_v2(from_path:str, to_path:str, __retry:int=5)->str:
     """
     上传文件到OBS（适用于大于5G大文件上传）
     :param from_path: 文件/文件夹的完整路径
@@ -121,9 +126,66 @@ def upload_file_v2(from_path:str, to_path:str)->str:
             raise Exception(f"上传OBS失败, resp:{resp.status} {resp.errorCode} {resp.errorMessage} {resp.requestId}")
     except Exception as e:
         print('Obs > 上传失败' + traceback.format_exc())
-        raise e
+        if __retry > 0:
+            sleep(1)
+            return upload_file_v2(from_path=from_path, to_path=to_path, __retry=__retry-1)
+        else:
+            raise e
     else:
         return urljoin(os.getenv("OBS_URLBASE"), to_path)
     finally:
-        # 关闭obsClient
+        obsClient.close()
+
+def download_file_by_url(obj_url:str, save_dir:str)->str:
+    """
+    download file from obs by url
+
+    :param obj_url: obs object url
+    :param save_dir: local save directory
+    :return: local file path
+    """
+    # 提取URL对象路径
+    if "obs" not in obj_url:
+        raise ValueError("obs object url invalid")
+    from_path = obj_url.split(".com")[1]
+    if from_path.startswith(r"/"):
+        from_path = from_path[1:]
+    filename = os.path.basename(from_path)
+    to_path = os.paht.join(save_dir, filename)
+    
+    # 下载处理
+    print(f"download_file_by_url > from_path:{from_path} to_path: {to_path}")
+    try:
+        # 创建obsClient实例
+        # 如果使用临时AKSK和SecurityToken访问OBS，需要在创建实例时通过security_token参数指定securityToken值
+        obsClient = ObsClient(access_key_id=ak, secret_access_key=sk, server=server)
+        bucketName = bucket
+        objectKey = from_path
+        # 下载到本地的路径,包含本地文件名称的全路径
+        downloadFile = to_path
+        # 分段下载的并发数
+        taskNum = 5
+        # 分段的大小
+        partSize = 10 * 1024 * 1024
+        # True表示开启断点续传
+        enableCheckpoint = True
+        # 断点续传下载对象
+        # resp = obsClient.downloadFile(bucketName, objectKey, downloadFile, partSize, taskNum, enableCheckpoint)
+        # 回调处理进度
+        resp = obsClient.downloadFile(bucketName, objectKey, downloadFile, partSize, taskNum, enableCheckpoint, progressCallback=callback)
+
+        # 返回码为2xx时，接口调用成功，否则接口调用失败
+        if resp.status < 300:
+            print('Obs > download_file_by_url Succeeded')
+        else:
+            print('Obs > [!] download_file_by_url Failed')
+            print('requestId:', resp.requestId)
+            print('errorCode:', resp.errorCode)
+            print('errorMessage:', resp.errorMessage)
+    except Exception as e:
+        print('download_file_by_url > Download File Failed', traceback.format_exc())
+        raise e
+    else:
+        return to_path
+    finally:
         obsClient.close()
