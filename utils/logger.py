8dfe4adcc5f43a99cacf62f6cefa7ca90a68e84d
AInoriex <xiangyuhao@52tt.com>
2024-11-23 23:21:00 +0800

    [feat] 日志单例模式；

--- utils/logger.py
+++ utils/logger.py
@@ -67,51 +67,65 @@ def init_logger(name):
 
     return logger
 
-def init_loguru():
-    """
-    Initializes a logger with loguru package.
 
-    The logger is configured to have different log levels based on the DEBUG environment variable.
-    If DEBUG is set to 'True', the log level is set to DEBUG; otherwise, it is set to INFO.
-    The logger outputs logs to both a file and the console. The log files are stored in a directory
-    specified by the LOG_PATH environment variable, organized by date.
+class Logger():
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(Logger, cls).__new__(cls, *args, **kwargs)
+        print("Logger已初始化")
+        return cls._instance
 
-    Args:
-        None
+    def __init__(self):
+        self.logger = None
 
-    Returns:
-        logging.Logger: The configured logger.
-    """
-    from loguru import logger
-    log_date = time.strftime("%Y-%m-%d", time.localtime())
-    log_time = time.strftime("%Y%m%d%H%M%S", time.localtime())
+    def init_loguru(self):
+        """
+        Initializes a logger with loguru package.
 
-    log_dir = os.path.join(os.getenv('LOG_PATH'), log_date)
-    os.makedirs(log_dir, exist_ok=True)
+        The logger is configured to have different log levels based on the DEBUG environment variable.
+        If DEBUG is set to 'True', the log level is set to DEBUG; otherwise, it is set to INFO.
+        The logger outputs logs to both a file and the console. The log files are stored in a directory
+        specified by the LOG_PATH environment variable, organized by date.
 
-    filename = os.path.join(log_dir, f"{log_time}.log")
-    logger.add(
-        filename,
-        level="DEBUG" if os.getenv('DEBUG')=='True' else "INFO",
-        format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {file}:{line} | Process.{process} | {message}",
-        colorize=True,
-        # 完整错误信息
-        diagnose=True if os.getenv('DEBUG')=='True' else False,
-        # 错误信息回溯
-        backtrace=True,
-        # 启动队列处理(多进程)
-        enqueue=True, 
-        # 单个日志文件大小
-        rotation="500MB",
-        # 日志文件保留时间
-        retention="15 days",
-        # 日志文件压缩格式
-        compression="zip",
-    )
-    logger.info(f"初始化日志记录器成功, 路径:{filename}")
-    return logger
+        Args:
+            None
 
-logger = init_loguru()
+        Returns:
+            logging.Logger: The configured logger.
+        """
+        from loguru import logger
+        log_date = time.strftime("%Y-%m-%d", time.localtime())
+        log_time = time.strftime("%Y%m%d.%H-%M", time.localtime())
+        # 预留日志路径
+        log_dir = os.path.join(os.getenv('LOG_PATH'), log_date)
+        os.makedirs(log_dir, exist_ok=True)
+        # 日志配置
+        filename = os.path.join(log_dir, f"{log_time}.log")
+        logger.add(
+            filename,
+            level="DEBUG" if os.getenv('DEBUG')=='True' else "INFO",
+            format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {file}:{line} | Process.{process} | {message}",
+            colorize=True,
+            # 完整错误信息
+            diagnose=True if os.getenv('DEBUG')=='True' else False,
+            # 错误信息回溯
+            backtrace=True,
+            # 启动队列处理(多进程)
+            enqueue=True, 
+            # 单个日志文件大小
+            rotation="500MB",
+            # 日志文件保留时间
+            retention="15 days",
+            # 日志文件压缩格式
+            compression="zip",
+        )
+        logger.info(f"初始化日志记录器成功, 路径:{filename}")
+        self.logger = logger
+
+g = Logger()
+g.init_loguru()
+logger = g.logger
 
 if __name__ == "__main__":
     logger.debug("这是一条测试日志")
