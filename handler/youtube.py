42e85a257efa5b184d664aa72a88e842ae9e4e2e
AInoriex <xiangyuhao@52tt.com>
2024-11-27 18:04:14 +0800

    [feat] 补充语言映射；

--- handler/youtube.py
+++ handler/youtube.py
@@ -124,6 +124,18 @@ def get_cloud_save_path_by_language(save_path:str, lang_key:str)->str:
         "vi": "Vietnam",		# 越南语
         "yue": "Yueyu",			# 粤语
         "zh": "Zhongwen",		# 中文
+        "nl": "language/Helanyu",       # 荷兰语
+        "tr": "language/Tuerqiyu",      # 土耳其语
+        "sv": "language/Ruidianyu",     # 瑞典语
+        "bg": "language/Baojialiyayu",  # 保加利亚语
+        "ro": "language/Luomaniyayu",   # 罗马尼亚语
+        "cs": "language/Jiekeyu",       # 捷克语
+        "fi": "language/Fenlanyu",      # 芬兰语
+        "hr": "language/Keluodiyayu",   # 克罗地亚语
+        "sk": "language/Siluofakeyu",   # 斯洛伐克
+        "da": "language/Danmaiyu",      # 丹麦语
+        "ta": "language/Taimieryu",     # 泰米尔语
+        "uk": "language/Wukelanyu",     # 乌克兰语
         "test": f"Unclassify/test",             # 测试数据
         "unknown": f"Unclassify/{lang_key}",    # 其他
     }
