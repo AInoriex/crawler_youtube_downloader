bb26c5b4200ef0da8d7a117e62a7ef12d692cb2c
AInoriex <xiangyuhao@52tt.com>
2024-12-02 16:52:54 +0800

    [feat] 补充语言；

--- handler/youtube.py
+++ handler/youtube.py
@@ -113,18 +113,17 @@ def get_cloud_save_path_by_language(save_path:str, lang_key:str)->str:
 		"it": "Yidaliyu",		# 意大利语
         "ja": "Riyu",			# 日语
         "ko": "Hanyu",			# 韩语
-        "mn": "Mengguyu",		# 蒙语/蒙古语
         "ms": "Malaiyu",		# 马来语
         "nan": "Minnanyu",		# 闽南语
 		"pl": "Bolanyu",		# 波兰语
 		"pt": "Putaoyayu",		# 葡萄牙语
         "ru": "Eyu",			# 俄语
         "th": "Taiyu",			# 泰语
-        "tl": "Tajialuyu",		# 他加禄语
         "vi": "Vietnam",		# 越南语
         "yue": "Yueyu",			# 粤语
         "zh": "Zhongwen",		# 中文
         "nl": "language/Helanyu",       # 荷兰语
+        "hi": "language/Yindiyu",       # 印地语
         "tr": "language/Tuerqiyu",      # 土耳其语
         "sv": "language/Ruidianyu",     # 瑞典语
         "bg": "language/Baojialiyayu",  # 保加利亚语
@@ -136,6 +135,10 @@ def get_cloud_save_path_by_language(save_path:str, lang_key:str)->str:
         "da": "language/Danmaiyu",      # 丹麦语
         "ta": "language/Taimieryu",     # 泰米尔语
         "uk": "language/Wukelanyu",     # 乌克兰语
+        "tl": "language/Tajialuyu",		# 他加禄语
+        "mn": "language/Mengguyu",		# 蒙语/蒙古语
+        "bo": "language/Zangyu",		# 藏语
+        "ug": "language/Weiwueryu",		# 维语/维吾尔语
         "test": f"Unclassify/test",             # 测试数据
         "unknown": f"Unclassify/{lang_key}",    # 其他
     }
