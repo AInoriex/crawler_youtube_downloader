d5b1a0d216e80ce88e304857cda028a672a49f93
AInoriex <xiangyuhao@52tt.com>
2025-01-07 17:31:04 +0800

    [feat] 新增pytubefix音频处理；

---
+++ handler/pytubefix.py
@@ -0,0 +1,125 @@
+from database.crawler_download_info import Video
+from handler.youtube_accout import YoutubeAccout
+from os import getenv
+from time import sleep, time
+from utils.logger import logger
+from utils.utime import get_now_time_string
+from utils.lark import alarm_lark_text
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
+
+def pytubefix_login_handler()->YoutubeAccout:
+    """
+    登录pytubefix
+    1. 获取可用账号
+    2. 登录账号获取token
+    3. token保存到文件
+    4. 更新状态
+
+    :return: YoutubeAccout
+    """
+    try:
+        # 1. 获取可用账号
+        account = YoutubeAccout()
+        account.get_new_account()
+        if account.id <= 0 or account.username == "" or account.password == "" or account.verify_email == "":
+            raise Exception("pytubefix_login_handler 获取新账号失败")
+
+        # 2. 登录账号获取token
+        print("pytubefix_login_handler > 正在自动登陆账号中，请稍后...")
+        token = account.account_auto_login(
+            url=getenv("CRAWLER_AUTO_LOGIN_API"),
+            username=account.username,
+            password=account.password,
+            verify_email=account.verify_email,
+        )
+        # token = {"yt-dlp_version":"2024.07.16","data":{"access_token":"ya29.a0ARW5m77x_oaR27kwgMoLkuilpbie_BiF92PXUWK6XJeB44yQjWjfBwnLWsTBeGcGz5A8q_vgdwUfSU51bm-X0lQYsxqx3I9Xv5cBBqfZcgsMs4JtP7k6ENBRrzcLPLQA5PWtHiSx0IE27eFAW56h8zUDnChidg_xFZxSdRAJuIhCB1q-2aHjaCgYKATUSARESFQHGX2MifKbYwHaU_fsIhaoN6Eddbg0187","expires":1736248157.389038,"refresh_token":"1//0ggz4_-Msif4SCgYIARAAGBASNwF-L9IrFTNepc7Qp2lMEKHdKavzEcjDeCAqyBy-OfxNR_pd5wYPAc3tZ3hzBKU7_a4ojuauLVw","token_type":"Bearer"}}
+        if not token:
+            raise Exception("pytubefix_login_handler 自动登陆账号失败, token为空")
+
+        # 封装pytubefix token格式
+        account.token = token["data"]
+        account.token["visitorData"] = None
+        account.token["po_token"] = None
+
+        # 3. token保存到文件
+        token_path = account.save_token_to_file(
+            token=account.token,
+            save_dir=f"./cache/pytubefix_{int(time())}"
+        )
+        if token_path == "":
+            raise Exception("pytubefix_login_handler 保存token失败")
+        account.token_path = token_path
+
+        # 4. 更新状态
+        account.login(is_login=True)
+    except Exception as e:
+        logger.error(f"pytubefix_login_handler > unknown error:{e}")
+        notice_text = f"[Youtube Crawler ACCOUNT | ERROR] 自动换号失败. \
+            \n\t登入方: {account.login_name} \
+            \n\t账密: {account.id} | {account.username} {account.password} \
+            \n\tERROR: {e} \
+            \n\t告警时间: {get_now_time_string()}"
+        alarm_lark_text(webhook=getenv("LARK_ERROR_WEBHOOK"), text=notice_text)
+        if account.id > 0:
+            account.login(is_login=False)
+        raise e
+    else:
+        notice_text = f"[Youtube Crawler ACCOUNT | INFO] 自动换号成功. \
+            \n\t登入方: {account.login_name} \
+            \n\t账密: {account.id} | {account.username} {account.password} \
+            \n\tToken Path: {account.token_path} \
+            \n\tToken Content: {account.token} \
+            \n\t告警时间: {get_now_time_string()}"
+        alarm_lark_text(webhook=getenv("LARK_INFO_WEBHOOK"), text=notice_text)
+
+def pytubefix_audio_handler(video:Video, save_path:str)->str|None:
+    """
+    pytubefix 下载音频
+    :param video: Video instance
+    :param save_path: save path of the downloaded video
+    :return: str|None the path of the downloaded video, None if failed
+    :raises ValueError: if failed to download the video
+    :raises Exception: if failed to get audio stream by get_by_itag
+    """
+    if not video.source_link:
+        logger.error(f"pytubefix_audio_handler > video source link is empty")
+        return None
+    proxies = {'http': getenv("HTTP_PROXY"),'https': getenv("HTTP_PROXY")} if getenv("HTTP_PROXY", "") != "" else None
+
+    yt = YouTube(video.source_link, on_progress_callback=on_progress, proxies=proxies, allow_oauth_cache=True, use_oauth=True)
+    # yt = YouTube(url, on_progress_callback=on_progress, proxies=proxies, use_oauth=True, token_file=r"cache\pytubefix_token\tokens.json")
+    logger.info(f"pytubefix_audio_handler > get the video {yt.title} from {yt.channel_url}")
+
+    # 按照 itag 获取 audio
+    format_type = "mp3"
+    
+    # Itag details according to https://web.archive.org/web/20200516070826/https://gist.github.com/Marco01809/34d47c65b1d28829bb17c24c04a0096f
+    # ::return:: <stream> | None
+    ys = None
+    if yt.streams.get_by_itag(251):
+        logger.debug("pytubefix_audio_handler > get WebM.Opus ~160 Kbps success")
+        format_type = "opus"
+        ys = yt.streams.get_by_itag(251)
+    elif yt.streams.get_by_itag(250):
+        logger.debug("pytubefix_audio_handler > get WebM.Opus ~70 Kbps success")
+        format_type = "opus"
+        ys = yt.streams.get_by_itag(250) 
+    elif yt.streams.get_by_itag(249):
+        logger.debug("pytubefix_audio_handler > get WebM.Opus ~50 Kbps success")
+        format_type = "opus"
+        ys = yt.streams.get_by_itag(249) 
+    elif yt.streams.get_by_itag(140):
+        logger.debug("pytubefix_audio_handler > get MP4.AAC 128 Kbps success")
+        format_type = "m4a"
+        ys = yt.streams.get_by_itag(140) 
+    else:
+        raise Exception("pytubefix_audio_handler > get audio stream by get_by_itag fail")
+    
+    # download_filepath = ys.download(output_path="download", filename_prefix="test-", skip_existing=True, max_retries=3, timeout=10)
+    filename=f"{video.vid}.{format_type}"
+    download_path = ys.download(output_path=save_path, filename=filename, skip_existing=True, max_retries=3, timeout=10)
+    if not download_path:
+        raise ValueError(f"pytubefix_audio_handler > download the video {video.source_link} fail")
+    logger.debug(f"pytubefix_audio_handler > 文件已下载到:{download_path}")
+    return download_path
--- ytb_download_pipeline.py
+++ ytb_download_pipeline.py
@@ -15,7 +15,7 @@ from utils.utime import random_sleep, get_now_time_string, format_second_to_time
 from utils.file import get_file_size
 from utils.lark import alarm_lark_text
 from utils.ip import get_local_ip, get_public_ip
-# from utils import logger as ulogger
+from database.crawler_download_info import Video
 # from handler.youtube_accout import YoutubeAccout
 # from handler.bilibili import download as bilibili_download
 # from handler.ximalaya import download as ximalaya_download
@@ -81,14 +81,14 @@ def youtube_sleep(is_succ:bool, run_count:int, download_round:int):
         else:
             random_sleep(rand_st=60, rand_range=20)
 
-def download_with_yt_dlp(video, save_path):
+def download_with_yt_dlp(video:Video, save_path:str):
     from handler.yt_dlp import download_by_watch_url
     download_path = download_by_watch_url(video, save_path=save_path)
     if download_path == "":
         raise("download_with_yt_dlp download_path empty")
     return download_path
 
-def download_with_tubedown(video, save_path):
+def download_with_tubedown(video:Video, save_path:str):
     # 1 解析真实视频url
     from handler.youtube import get_youtube_vid, get_mime_type
     from handler.tubedown import extract_download_url, download_resource
@@ -106,13 +106,13 @@ def download_with_tubedown(video, save_path):
     download_path = download_resource(
         url = video_url,
         filename = filename,
-        proxies = {'http': getenv("HTTP_PROXY"),'https': getenv("HTTP_PROXY")} if getenv("HTTP_PROXY", "") != "" else {}
+        proxies = {'http': getenv("HTTP_PROXY"),'https': getenv("HTTP_PROXY")} if getenv("HTTP_PROXY", "") != "" else None
     )
     if download_path == "":
         raise ValueError("download_with_tubedown get empty download file")
     return download_path
 
-def download_with_rapidapi(video, save_path):
+def download_with_rapidapi(video:Video, save_path:str):
     from handler.youtube import get_youtube_vid, get_mime_type
     from handler.tubedown import download_resource
     # 1 解析真实视频url
@@ -133,7 +133,7 @@ def download_with_rapidapi(video, save_path):
         raise ValueError("download_with_rapidapi get empty download file")
     return download_path
 
-def download_with_yt_api(video, save_path):
+def download_with_yt_api(video:Video, save_path:str):
     from handler.yt_api import ytapi_handler
     download_path = ytapi_handler(
         video_id=get_youtube_vid(video.source_link),
@@ -143,7 +143,14 @@ def download_with_yt_api(video, save_path):
         raise ValueError("download_with_yt_api get empty download file")
     return download_path
 
-def youtube_download_handler(video, save_path):
+def download_with_pytubefix_audio(video:Video, save_path:str):
+    from handler.pytubefix import pytubefix_audio_handler
+    download_path = pytubefix_audio_handler(video, save_path)
+    if not download_path:
+        raise ValueError("download_with_pytubefix get empty download file")
+    return download_path
+
+def youtube_download_handler(video:Video, save_path):
     if getenv("YTB_DOWNLOAD_MODE", "") == "tubedown":
         logger.info("youtube_download_handler > 当前下载模式: tubedown")
         return download_with_tubedown(video, save_path)
@@ -156,6 +163,9 @@ def youtube_download_handler(video, save_path):
     elif getenv("YTB_DOWNLOAD_MODE", "") == "yt_dlp":
         logger.info("youtube_download_handler > 当前下载模式: yt_dlp")
         return download_with_yt_dlp(video, save_path)
+    elif getenv("YTB_DOWNLOAD_MODE", "") == "pytubefix_audio":
+        logger.info("youtube_download_handler > 当前下载模式: pytubefix_audio")
+        return download_with_pytubefix_audio(video, save_path)
     else:
         logger.warning("youtube_download_handler > 未配置下载模式, 默认: yt_dlp")
         return download_with_yt_dlp(video, save_path)
@@ -249,7 +259,7 @@ def main_pipeline(pid):
                 \n\t下载均速: {file_size/spend_download_time:.2f}M/s , 上传均速: {file_size/spend_upload_time:.2f}M/s \
                 \n\tIP: {LOCAL_IP} | {get_public_ip()}"
             logger.info(notice_text)
-            alarm_lark_text(webhook=getenv("LARK_NOTICE_WEBHOOK"), text=notice_text)
+            alarm_lark_text(webhook=getenv("LARK_INFO_WEBHOOK"), text=notice_text)
 
             logger.success(f"Pipeline > 进程 {pid} 处理任务 {video_id} 完毕")
         except KeyboardInterrupt:
@@ -281,7 +291,7 @@ def main_pipeline(pid):
                 \n\t告警时间: {get_now_time_string()} \
                 \n\tStack Info: {format_exc()[-500:]}"
             logger.error(notice_text)
-            alarm_lark_text(webhook=getenv("LARK_NOTICE_WEBHOOK"), text=notice_text)
+            alarm_lark_text(webhook=getenv("LARK_INFO_WEBHOOK"), text=notice_text)
             # 失败过多直接退出
             if continue_fail_count > LIMIT_FAIL_COUNT:
                 logger.error(f"Pipeline > 进程 {pid} 失败过多超过{continue_fail_count}次, 异常退出")
